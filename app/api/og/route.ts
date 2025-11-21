2ae49b389710e673d447cd47d44efe857b3eafe7
EL <110147776+hallosayael@users.noreply.github.com>
2025-11-21 12:43:43 +0700

    Update route.ts

--- app/api/og/route.ts
+++ app/api/og/route.ts
@@ -1,16 +1,17 @@
-/* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "next/og";
+import { ImageResponse } from "next/server";
 
 export const runtime = "edge";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const fortune = searchParams.get("fortune") || "Your lucky fortune awaits!";
+  const fortune = searchParams.get("fortune") || "Your fortune awaits!";
 
+  // Return OG image using pure HTML (no JSX)
   return new ImageResponse(
-    (
-      <div
-        style={{
+    {
+      type: "div",
+      props: {
+        style: {
           width: "100%",
           height: "100%",
           display: "flex",
@@ -21,28 +22,33 @@ export async function GET(req: Request) {
           fontSize: 40,
           fontWeight: 600,
           padding: "30px",
-        }}
-      >
-        <img
-          src="https://i.ibb.co/dtQ2BrV/anime-girl.png"
-          width="240"
-          height="240"
-          alt="anime"
-          style={{ borderRadius: "20px" }}
-        />
-        <div
-          style={{
-            marginTop: "30px",
-            textAlign: "center",
-            maxWidth: "600px",
-            fontSize: "32px",
-            color: "#5a3825",
-          }}
-        >
-          {fortune}
-        </div>
-      </div>
-    ),
+        },
+        children: [
+          {
+            type: "img",
+            props: {
+              src: "https://i.ibb.co/dtQ2BrV/anime-girl.png",
+              width: 240,
+              height: 240,
+              style: { borderRadius: "20px" },
+            },
+          },
+          {
+            type: "div",
+            props: {
+              style: {
+                marginTop: "30px",
+                textAlign: "center",
+                maxWidth: "600px",
+                fontSize: "32px",
+                color: "#5a3825",
+              },
+              children: fortune,
+            },
+          },
+        ],
+      },
+    },
     {
       width: 1200,
       height: 630,
