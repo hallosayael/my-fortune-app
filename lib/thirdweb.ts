ec9559df0bc190bf06eed50ee6b326c42c16da16
EL <110147776+hallosayael@users.noreply.github.com>
2025-11-21 12:54:55 +0700

    Delete lib/thirdweb.ts

--- lib/thirdweb.ts
+++
@@ -1,8 +0,0 @@
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-
-const sdk = new ThirdwebSDK({
-  // sesuaikan dengan chain yang kamu pakai
-  clientId: process.env.THIRDWEB_CLIENT_ID!,
-});
-
-export default sdk;
